5db9467f0340b66b38753703b16fc8a938fec1fa
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-06 08:12:10 +0900

    JSXで構造を作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input plceholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+
+      <div>
+        <ul>
+          <div>
+            <li>ああああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+        <ul>
+          <div>
+            <li>いいいいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <div>
+            <li>ううううう</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+        <ul>
+          <div>
+            <li>ええええええ</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
