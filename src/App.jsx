60fd37c66054ffd0f52d4d08c092d91366230906
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-06 08:47:22 +0900

    Reactの実装を意識したモックに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "あああああ",
+    "いいいいい"
+  ]);
+  const [completeTodos, setCompleteTodos] = useState([
+    "ううううう",
+    "えええええ"
+  ]);
   return (
     <>
       <div className="input-area">
@@ -11,34 +20,29 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>ああああああ</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-        </ul>
-        <ul>
-          <div className="list-row">
-            <li>いいいいいい</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
 
       <div className="complete-area">
         <p className="title">完了したTODO</p>
         <ul>
-          <div className="list-row">
-            <li>ううううう</li>
-            <button>戻す</button>
-          </div>
-        </ul>
-        <ul>
-          <div className="list-row">
-            <li>ええええええ</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
